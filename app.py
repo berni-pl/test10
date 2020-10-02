f467b12e8ab4ecc7d2b570cafd951c448d4e7533
wojciber <bernard.wojcik@orange.com>
2020-10-02 12:12:34 +0200

    fdfd

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
-from db import db
 
 from security import authenticate,identity
 from resources.user import UserRegister
@@ -14,9 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='berni'
 api=Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
+
 
 jwt=JWT(app,authenticate,identity)
 		
@@ -32,5 +29,6 @@ api.add_resource(StoreList, '/stores')
 
 
 if __name__ == '__main__':
+    from db import db
 	db.init_app(app)
 	app.run(port=5678,debug=True)
